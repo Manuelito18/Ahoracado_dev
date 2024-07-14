a38d3ecf110b6b63b151d6aa9cc8cb3e0549cc72
DevManuelito <jomafersan14@gmail.com>
2024-07-14 17:41:34 -0500

    Enlaze ided

--- src/game/main.java
+++ src/game/main.java
@@ -115,7 +115,9 @@ public class main extends javax.swing.JFrame {
     }//GEN-LAST:event_jButton2ActionPerformed
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
-        // TODO add your handling code here:
+        juego jn=new juego();
+        jn.setVisible(true);
+        this.setVisible(false);
     }//GEN-LAST:event_jButton3ActionPerformed
 
     private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
